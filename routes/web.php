3f0af6776395af0c5c9f19b65327cc2ea82fa4db
shaimaayosef <shimaa.sayed8888@gmail.com>
2024-06-13 15:05:02 +0300

    day 12

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use Illuminate\Support\Facades\Auth; // Import the Auth class
+
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('hello', function () {
     return view('welcome');
 });
+
+Auth::routes(['verify'=>true]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->middleware('verified')->name('home');
